32ee9d775e1a1d2824d76303f54e551b3af539e0
pinkoa2 <apinkow27@gmail.com>
2023-06-30 20:43:00 -0400

    feat: completed day1

--- 2020/src/day/day1.rs
+++ 2020/src/day/day1.rs
@@ -1,7 +1,48 @@
-fn part1() {}
-fn part2() {}
+use crate::util;
+
+fn part1(filetype: util::FileType) -> i32 {
+    let input: String = util::read_file(String::from("day1"), filetype);
+    let split_string: Vec<&str> = input.split("\n").collect();
+
+    for value in &split_string {
+        let partner = 2020 - value.parse::<i32>().unwrap();
+        if split_string.contains(&(&partner.to_string()[..])) {
+            return partner * value.parse::<i32>().unwrap();
+        }
+    }
+    return 0;
+}
+
+fn part2(filetype: util::FileType) -> i32 {
+    let input: String = util::read_file(String::from("day1"), filetype);
+    let split_string: Vec<&str> = input.split("\n").collect();
+    for value1 in &split_string {
+        for value2 in &split_string {
+            for value3 in &split_string {
+                let v1 = value1.parse::<i32>().unwrap();
+                let v2 = value2.parse::<i32>().unwrap();
+                let v3 = value3.parse::<i32>().unwrap();
+                if v1 + v2 + v3 == 2020 {
+                    return v1 * v2 * v3;
+                }
+            }
+        }
+    }
+    return 1;
+}
 
 pub fn run() {
-    part1();
-    part2();
+    println!("Solution part1: {}", part1(util::FileType::Input));
+    println!("Solution part2: {}", part2(util::FileType::Input));
+}
+
+#[test]
+fn test_part_1() {
+    let result = part1(util::FileType::Test);
+    assert_eq!(result, 514579);
+}
+#[test]
+fn test_part_2() {
+    let result = part2(util::FileType::Test);
+    assert_eq!(result, 241861950)
 }
--- 2020/src/util.rs
+++ 2020/src/util.rs
@@ -1,1 +1,17 @@
-pub fn read_file(filename: String) {}
+use std::fs;
+
+pub enum FileType {
+    Input,
+    Test,
+}
+
+pub fn read_file(filename: String, filetype: FileType) -> String {
+    let file_prefix = match filetype {
+        FileType::Input => "input",
+        FileType::Test => "test",
+    };
+    let file_path = format!("./src/{file_prefix}/{filename}.in");
+    let file: String = fs::read_to_string(file_path).expect("Failed to read from file {filename}");
+    let trimed_file = file.trim();
+    return trimed_file.to_string();
+}
