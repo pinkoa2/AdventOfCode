d6780518101b33619c545b2e6641bfd2091e59de
pinkoa2 <apinkow27@gmail.com>
2023-07-02 11:29:40 -0400

    feat: completed day6

---
+++ 2020/src/day/day6.rs
@@ -0,0 +1,54 @@
+use crate::util::*;
+use std::collections::HashSet;
+
+fn part1(filetype: FileType) -> usize {
+    let input: String = read_file(String::from("day6"), filetype);
+
+    let groups = input.split("\n\n");
+
+    let mut result = 0;
+    for people in groups {
+        let set_of_answers = people
+            .chars()
+            .filter(|c| c.is_ascii_alphabetic())
+            .collect::<Vec<char>>();
+        let count: HashSet<char> = HashSet::<char>::from_iter(set_of_answers);
+        result += count.len()
+    }
+
+    result
+}
+
+fn part2(filetype: FileType) -> usize {
+    let input: String = read_file(String::from("day6"), filetype);
+
+    let groups = input.split("\n\n").collect::<Vec<&str>>();
+
+    let mut result = 0;
+    for group in groups {
+        let mut hashset: HashSet<char> = (b'a'..b'{').map(|c| c as char).collect();
+        for people in group.split("\n") {
+            let inner_hashset: HashSet<char> = HashSet::<char>::from_iter( people.chars() );
+            hashset = hashset.intersection(&inner_hashset).cloned().collect();
+        }
+        result += hashset.len();
+    }
+    result
+}
+
+pub fn run() {
+    println!("Solution to part1: {}", part1(FileType::Input));
+    println!("Solution to part2: {}", part2(FileType::Input));
+}
+
+#[test]
+fn test_part1() {
+    let result = part1(FileType::Test);
+    assert_eq!(result, 11)
+}
+
+#[test]
+fn test_part2() {
+    let result = part2(FileType::Test);
+    assert_eq!(result, 6)
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,11 +1,13 @@
 pub mod day;
 pub mod util;
-use day::*;
+
+use crate::day::*;
 
 fn main() {
     // day1::run();
     // day2::run();
     // day3::run();
     // day4::run();
-    day5::run();
+    // day5::run();
+    day6::run();
 }
