9c86bbb3dd946102bafa3c48da001d3db9f2edd0
pinkoa2 <apinkow27@gmail.com>
2023-07-07 18:21:24 +0100

    feat: complete day 9

---
+++ 2020/src/bin/day9.rs
@@ -0,0 +1,88 @@
+use aoc2020::util::{read_file, FileType};
+use std::collections::HashSet;
+
+fn part1(filetype: FileType, preamble: usize) -> u128 {
+    let input = read_file(String::from("day9"), filetype);
+    let splitted: Vec<&str> = input.split("\n").collect();
+
+    let sequence: Vec<u128> = splitted
+        .iter()
+        .map(|num| num.parse::<u128>().unwrap())
+        .collect();
+
+    for i in preamble..sequence.len() {
+        let current = sequence[i];
+        let spliced = &sequence[(i - preamble)..i];
+        let hashset: HashSet<&u128> = spliced.iter().collect();
+
+        let mut found_partner = false;
+        for num in spliced {
+            let lookup = match current.checked_sub(num.clone()) {
+                Some(val) => val,
+                None => 0,
+            };
+            if hashset.contains(&lookup) {
+                found_partner = true;
+            }
+        }
+        if !found_partner {
+            return current;
+        }
+    }
+    panic!("Should not reach here");
+}
+
+fn part2(filetype: FileType, preamble: usize) -> u128 {
+    let copy = match filetype {
+        FileType::Test => FileType::Test,
+        FileType::Input => FileType::Input,
+    };
+
+    let value = part1(copy, preamble);
+    let input = read_file(String::from("day9"), filetype);
+
+    let splitted: Vec<&str> = input.split("\n").collect();
+    let sequence: Vec<u128> = splitted
+        .iter()
+        .map(|num| num.parse::<u128>().unwrap())
+        .collect();
+
+    let mut starting = 0;
+    let mut ending: usize;
+    loop {
+        ending = starting + 1;
+        'inner: loop {
+            let sum_over_range: u128 = sequence[starting..=ending].iter().sum();
+
+            if sum_over_range == value {
+                return sequence[starting] + sequence[ending - 1];
+            }
+
+            if sum_over_range > value {
+                break 'inner;
+            }
+            ending += 1;
+        }
+        starting += 1;
+    }
+}
+
+fn main() {
+    println!("Solution to part1 is {}", part1(FileType::Input, 25));
+    println!("Solution to part2 is {}", part2(FileType::Input, 25));
+}
+
+#[test]
+fn test_part_1() {
+    let test = part1(FileType::Test, 5);
+    assert_eq!(test, 127);
+    let input = part1(FileType::Input, 25);
+    assert_eq!(input, 393911906);
+}
+#[test]
+fn test_part_2() {
+    let test = part2(FileType::Test, 5);
+    assert_eq!(test, 62);
+    let input = part2(FileType::Input, 25);
+    assert_eq!(input, 44522522);
+}
